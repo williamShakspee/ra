f4223c26c9eb605bad2716b688e23335a33de53e
williamShakspee <gabrielgttl20@gmail.com>
2023-10-23 22:22:46 -0300

    added js

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Navbar/>
+              <Routes>
+              </Routes>
+            
+        </BrowserRouter>
+       
+    </div>
+  );
+}
+export default App;
